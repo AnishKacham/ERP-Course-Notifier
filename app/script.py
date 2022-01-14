55f5dba63f44ee46fbf17efc5d0095fb4d35a75c
AnishKacham <f20190091@hyderabad.bits-pilani.ac.in>
2022-01-15 04:40:13 +0530

    [FIX] : Tab Switching

--- app/script.py
+++ app/script.py
@@ -8,46 +8,47 @@ from py_console import console
 import time
 
 #_ path for the webdriver
-PATH = "/home/jonny/Documents/Projects/funs/chromedriver"
+PATH = "/home/anish/environments/selen/chromedriver"
 
 #_ options before starting the task
 chromium_options = Options()
-chromium_options.add_argument("--user-data-dir=/home/jonny/Documents/Projects/funs/app/userdata") ## This will be an empty folder initially 
-## and cookie will be sotred here for later use
+chromium_options.add_argument("--user-data-dir=/home/anish/enivronments/selen/app/userdata") #* This will be an empty folder initially 
+#* and cookie will be sotred here for later use
 # chromium_options.add_argument("--headless")
 
 #_ start the webdriver
 driver = webdriver.Chrome(PATH,options=chromium_options)
-driver.get("https://sis.erp.bits-pilani.ac.in/psc/sisprd/EMPLOYEE/SA/c/NUI_FRAMEWORK.PT_LANDINGPAGE.GBL") ## opens up the particular page
+driver.get("https://erp.bits-pilani.ac.in/") #* opens up the particular page
 
 #_ find sutdent system and click on it
-# table = driver.find_elements_by_tag_name("tr")
-# console.success("[SUCCESS] : student system row found")
+table = driver.find_elements_by_tag_name("tr")
+console.success("[SUCCESS] : Student System row found")
 
-# studentSystemButton = table[2].find_element_by_class_name("btn")
-# studentSystemButton.click()
+studentSystemButton = table[2].find_element_by_class_name("btn")
+studentSystemButton.click()
 
-#! Home ERP Page Reached
-## wait for it to load completely and click on registration page button
+## Home ERP Page Reached
+#* wait for it to load completely and click on registration page button
+#! Opens in a new tab
 try:
-    console.log("trying")
-    driver.implicitly_wait(10)
-    registrationPageBtn = driver.find_element_by_id("win0divPTNUI_LAND_REC_GROUPLET$2")
-    print(registrationPageBtn)
-    registrationPageBtn.click()
-    # registrationPageBtn = WebDriverWait(driver,30).until(
-    #     EC.presence_of_element_located((By.ID,"win0divPTNUI_LAND_REC_GROUPLET$2"))
-    # )
-    # console.success("[SUCCESS] : Logged In to ERP. Home Page Reached")
-    # registrationPageBtn.click()
-    
-    
-except Exception as e: # work on python 3.x
-    console.error(str(e))
+    #_ Switch tabs
+    windowHandles = driver.window_handles
+    driver.switch_to.window(windowHandles[1])
+    console.success("[SUCCESS] : Logged In to ERP. Home Page Reached")
+
+    registrationPageBtn = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.ID,"win0divPTNUI_LAND_REC_GROUPLET$2"))
+    )    
+    registrationPageBtn.click()  
+    console.success("[SUCCESS] : Registration Page Reached") 
+except:
+    #TODO FINISH THIS LATER
+    console.warn("[WARNING] : Couldn't fetch link. Using Hardcoded link . . .")
+    #driver.get("https://sis.erp.bits-pilani.ac.in/psc/sisprd_newwin/EMPLOYEE/SA/c/NUI_FRAMEWORK.PT_AGSTARTPAGE_NUI.GBL?CONTEXTIDPARAMS=TEMPLATE_ID%3aPTPPNAVCOL&scname=ADMN_REGISTRATION&PTPPB_GROUPLET_ID=STUDENT_REGISTRATION&CRefName=ADMN_NAVCOLL_3")
     
 
-## now we have to wait until the clicked page loads 
-## hence we use a try and except statement
+#* now we have to wait until the clicked page loads 
+#* hence we use a try and except statement
 # try:
 #     beginnerpytut = WebDriverWait(driver,10).until(
 #         EC.presence_of_element_located((By.LINK_TEXT,"Beginner Python Tutorials"))
