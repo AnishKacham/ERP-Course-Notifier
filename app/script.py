dc0b14f889c16da39aa6022732a78c2b967de5b8
Joe2k <jonathansamuel2k@gmail.com>
2022-01-15 03:17:18 +0530

    [FIX] nub stuff

--- app/script.py
+++ app/script.py
@@ -8,38 +8,42 @@ from py_console import console
 import time
 
 #_ path for the webdriver
-PATH = "/home/anish/environments/selen/chromedriver"
+PATH = "/home/jonny/Documents/Projects/funs/chromedriver"
 
 #_ options before starting the task
 chromium_options = Options()
-chromium_options.add_argument("--user-data-dir=/home/anish/enivronments/selen/app/userdata") ## This will be an empty folder initially 
+chromium_options.add_argument("--user-data-dir=/home/jonny/Documents/Projects/funs/app/userdata") ## This will be an empty folder initially 
 ## and cookie will be sotred here for later use
 # chromium_options.add_argument("--headless")
 
 #_ start the webdriver
 driver = webdriver.Chrome(PATH,options=chromium_options)
-driver.get("https://erp.bits-pilani.ac.in/") ## opens up the particular page
+driver.get("https://sis.erp.bits-pilani.ac.in/psc/sisprd/EMPLOYEE/SA/c/NUI_FRAMEWORK.PT_LANDINGPAGE.GBL") ## opens up the particular page
 
 #_ find sutdent system and click on it
-table = driver.find_elements_by_tag_name("tr")
-console.success("[SUCCESS] : student system row found")
+# table = driver.find_elements_by_tag_name("tr")
+# console.success("[SUCCESS] : student system row found")
 
-studentSystemButton = table[2].find_element_by_class_name("btn")
-studentSystemButton.click()
+# studentSystemButton = table[2].find_element_by_class_name("btn")
+# studentSystemButton.click()
 
 #! Home ERP Page Reached
 ## wait for it to load completely and click on registration page button
 try:
     console.log("trying")
-    registrationPageBtn = WebDriverWait(driver,30).until(
-        EC.presence_of_element_located((By.ID,"win0divPTNUI_LAND_REC_GROUPLET$2"))
-    )
-    console.success("[SUCCESS] : Logged In to ERP. Home Page Reached")
+    driver.implicitly_wait(10)
+    registrationPageBtn = driver.find_element_by_id("win0divPTNUI_LAND_REC_GROUPLET$2")
+    print(registrationPageBtn)
     registrationPageBtn.click()
+    # registrationPageBtn = WebDriverWait(driver,30).until(
+    #     EC.presence_of_element_located((By.ID,"win0divPTNUI_LAND_REC_GROUPLET$2"))
+    # )
+    # console.success("[SUCCESS] : Logged In to ERP. Home Page Reached")
+    # registrationPageBtn.click()
     
     
-except:
-    console.error("[TIMEOUT] : Page took too long to load Quitting . . .")
+except Exception as e: # work on python 3.x
+    console.error(str(e))
     
 
 ## now we have to wait until the clicked page loads 
